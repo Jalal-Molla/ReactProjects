1fde29c6445ed3fa6de28d9d10c3e7ac8a0c9cef
Md Jalal Molla <98682903+Jalal-Molla@users.noreply.github.com>
2024-12-03 11:34:02 +0600

    Delete Testing directory

--- Testing/Copyright.js
+++
@@ -1,5 +0,0 @@
-export default function Copyright({year}){
-    return(
-        <p style={{color:"green"}}><u>You are in the year of{' '+year}</u></p>
-    );
-}
--- Testing/FancyText.js
+++
@@ -1,5 +0,0 @@
-export default function FancyText({title,text}){
-    return title
-    ? <h1>{text}</h1>
-    :<h3>{text}</h3>
-}
