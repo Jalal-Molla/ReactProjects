ab22c5843370ae668a8c2070c57294c3bec3fb92
Md Jalal Molla <98682903+Jalal-Molla@users.noreply.github.com>
2024-12-03 10:54:27 +0600

    Create data.js

---
+++ BusApp/data.js
@@ -0,0 +1,49 @@
+const buses=[
+    {
+        name:"HANIF",
+        position:1,
+        category:'Hino-Akj,Hino-1J,Hino-RM2,VOLVO,MERCEDEZ,Leyland,Fuso,Eicher',
+        descpription:'HANIF bus is the most populer bus in the Bangladesh,I found many lovers of the bus.and me also lover of it',
+        avgspeed:'125/kmp',
+        image:"hanif.jpg",
+        imogi:'😍'
+
+    },
+
+    {
+        name:"SAKURA",
+        position:2,
+        category:'Hino-Akj,HYUNDAI',
+        descpription:'SAKURA is the most populer bus in the road of Barishal to Dhaka.and me also lover of it',
+        avgspeed:'120/kmp',
+        image:"sakura.jpeg",
+        imogi:'😘'
+
+    },
+
+    {
+        name:"ENA",
+        position:3,
+        category:'Hino-Akj,Hino-1J,HYUNDAI,MERCEDEZ,Leyland,Fuso',
+        descpription:'ENA bus is the most populer bus in the Road of Sylhet to Dhaka,I found many lovers of the bus.and me also hater of it',
+        avgspeed:'110/kmp',
+        image:"ena.jpg",
+        imogi:'😤'
+
+    },
+    {
+        name:"SHYAMOLI",
+        position:4,
+        category:'Hino-Akj,HYUNDAI,Hino-RM2,VOLVO,Leyland',
+        descpription:'SHYAMOLI bus is the most populer bus in the Bangladesh,I found many lovers of the bus.and me also lover of it',
+        avgspeed:'100/kmp',
+        image:"shymoli.jpg",
+        imogi:'😵‍💫'
+
+    },
+
+
+
+
+];
+export default buses;
