f685842e1c155d4b3f241e8a557a2e66892279f9
Md Jalal Molla <98682903+Jalal-Molla@users.noreply.github.com>
2024-12-03 11:36:33 +0600

    Add files via upload

---
+++ Testing/src/Copyright.js
@@ -0,0 +1,5 @@
+export default function Copyright({year}){
+    return(
+        <p style={{color:"green"}}><u>You are in the year of{' '+year}</u></p>
+    );
+}
---
+++ Testing/src/Data.js
@@ -0,0 +1,71 @@
+//here is my list component
+export const people=[
+    {
+        id:1,
+        name:"Md Jala Molla",
+        profession:"DeepMinder",
+        programming:"Java",
+        framworks:"Spring",
+        imogi:'😀'
+
+    },
+
+    {
+        id:2,
+        name:"Md Easmile Molla",
+        profession:"DeepMinder",
+        programming:"Python",
+        framworks:"Django",
+        imogi:'😍'
+
+    },
+
+    {
+        id:3,
+        name:"Md Easmile Molla",
+        profession:"DeepMinder",
+        programming:"JavaScript",
+        framworks:"React on fire!",
+        imogi:'🥳'
+
+    },
+
+    {
+        id:4,
+        name:"Md Easmile Molla",
+        profession:"DeepMinder",
+        programming:"C++",
+        framworks:"JUICE",
+        imogi:'🤑'
+
+    },
+
+    {
+        id:5,
+        name:"Md Easmile Molla",
+        profession:"DeepMinder",
+        programming:"DateScience",
+        framworks:"CherryPy",
+        imogi:'😵‍💫'
+
+    },
+    {
+        id:6,
+        name:"Md Easmile Molla",
+        profession:"DeepMinder",
+        programming:"MachineLearning",
+        framworks:"PyTorch",
+        imogi:'😑'
+
+    },
+    {
+        id:7,
+        name:"Md Easmille Molla",
+        profession:"DeepMinder",
+        programming:"Security",
+        framworks:"Hacker",
+        imogi:'👺'
+
+    }
+
+];
